b6b43464c611c2a802587e8a2ac03fe7bb7bf802
Jouryjc <494208562@qq.com>
2021-08-29 21:41:15 +0800

    feat: add plugin handle

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ const { transformFromAst, transformSync } = require('@babel/core');
 const plugin = require('./plugin/index.js');
 const transformVue2to3 = require('./plugin/transform-vue2-options-to-vue3-composition')
 
-const sourceCode = `
-spliceText('我有一只小{0}，我从来都不{1}', '毛驴', '骑')    // 有一只小毛驴，我从来都不骑
-spliceText('我叫{0}，今年{1}岁，特长是{2}', '小余', 18, '睡觉') // 叫小余，今年18岁，特长是睡觉
-spliceText('有趣的灵魂')    // 有趣的灵魂
-`
+// const sourceCode = `
+// spliceText('我有一只小{0}，我从来都不{1}', '毛驴', '骑')    // 有一只小毛驴，我从来都不骑
+// spliceText('我叫{0}，今年{1}岁，特长是{2}', '小余', 18, '睡觉') // 叫小余，今年18岁，特长是睡觉
+// spliceText('有趣的灵魂')    // 有趣的灵魂
+// `
 
-const ast = parser.parse(sourceCode, {
-    sourceType: 'unambiguous'
-})
-const code = transformFromAst(ast, sourceCode, {
-    plugins: [
-        plugin
-    ]
-}).code
+// const ast = parser.parse(sourceCode, {
+//     sourceType: 'unambiguous'
+// })
+// const code = transformFromAst(ast, sourceCode, {
+//     plugins: [
+//         plugin
+//     ]
+// }).code
 
-console.log(code)
+// console.log(code)
 
-const code2 = transformSync(`import HelloWorld from './components/HelloWorld.vue'
+const code2 = transformSync(`
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
@@ -36,8 +37,6 @@ export default {
     }
   },
 
-  created () {},
-
   data () {
     return {
       lastName: 'Tom'
@@ -47,24 +46,32 @@ export default {
   computed: {
     name () {
       return this.firstName + this.lastName
-    }
+    },
+
+    secondName: {
+      get () {
+        return this.lastName + this.firstName
+      }
+    } 
   },
 
   watch: {},
 
   methods: {
-    sayHello () {
+    sayHello (aa) {
       console.log('say Hi')
+      return aaa
     }
   },
 
-  beforeDestroy () {
-    console.log('before destroy!')
+  beforeUnmount () {
+    console.log('before unmount!')
   }
   
 }`, {
-    plugins: [
-        plugin
-    ]
+  targets: "> 0.25%, not dead",
+  plugins: [
+    transformVue2to3
+  ]
 }).code
 console.log(code2)
--- plugin/transform-vue2-options-to-vue3-composition.js
+++ plugin/transform-vue2-options-to-vue3-composition.js
@@ -1,20 +1,149 @@
 const { declare } = require('@babel/helper-plugin-utils');
 
 module.exports = declare((api, options) => {
-    api.assertVersion(7);
-    const { types } = api;
+    api.assertVersion(7)
+    const { types, template } = api
+    const bodyContent = []
+
+    const propertyMap = {
+        props: {},
+        state: {}
+    }
+
+    function assetType(property, type) {
+        return types.isIdentifier(property?.key, { name: type })
+    }
+
+    /**
+     * 转换props
+     * options 中的 props 作为 composition api 的参数
+     */
+    function genDefineProps(property) {
+        bodyContent.push(
+            types.variableDeclaration(
+                'const',
+                [
+                    types.variableDeclarator(
+                        types.identifier('props'),
+                        types.callExpression(
+                            types.identifier('defineProps'),
+                            [
+                                property.value
+                            ]
+                        )
+                    )
+                ]
+            )
+        )
+    }
+
+    /**
+     * 转换data
+     * options api 中的 data 转成 composition api 中的 reactive 函数
+     */
+    function genReactiveData(property) {
+        const reactiveDataAST = template.ast(`const state = reactive();`);
+
+        reactiveDataAST.declarations[0].init.arguments.push(property.value.body.body[0].argument)
+
+        // const toRefsAST = template.ast(`toRefs(state)`);
+
+        bodyContent.push(
+            reactiveDataAST,
+            // toRefsAST
+        )
+    }
+
+    /**
+     * 转换computed
+     * options api 中的 computed 转成 composition api 中的 computed 函数
+     */
+    function genComputed(property) {
+        const properties = property.value.properties
+        const computedArr = []
+
+        properties.forEach(item => {
+            const fnName = item.key.name
+
+            let computedAST = null
+
+            if (types.isObjectMethod(item)) {
+                computedAST = template.ast(`const ${fnName} = computed(() => {})`)
+                computedAST.declarations[0].init.arguments[0].body = item.value.body
+            } else if (types.isObjectProperty(item)) {
+                computedAST = template.ast(`const ${fnName} = computed()`)
+                computedAST.declarations[0].init.arguments.push(item.value)
+            }
+
+            computedArr.push(computedAST)
+        })
+
+        bodyContent.push(...computedArr)
+    }
     
+    function genMethods (property) {
+        const properties = property.value.properties
+        const methodArr = []
+
+        properties.forEach(item => {
+            const fnName = item.key.name
+
+            let ast = null
+            
+            if (types.isObjectProperty(item)) {
+                ast = template.ast(`const ${fnName} = () => {}`)
+                ast.declarations[0].init.params = item.value.params
+                ast.declarations[0].init.arguments[0].body = item.value.body
+            }
+
+            methodArr.push(ast)
+        })
+
+        bodyContent.push(...methodArr)
+    }
+
     return {
         name: 'transform-vue2-options-to-vue3-composition-plugin',
-        pre (file) {
-            
+        pre(file) {
+            console.log(file)
         },
         visitor: {
-            Identifier (path, state) {
-                
+            // ObjectProperty (path) {
+            //     console.log(path)
+            // },
+            // ThisExpression: {
+            //     enter (path) {
+
+            //     }
+            // },
+            Program: {
+                exit(path) {
+                    const exportDefaultDeclaration = path.node.body.filter(item => types.isExportDefaultDeclaration(item))
+                    const properties = exportDefaultDeclaration[0].declaration.properties
+
+                    properties.forEach(property => {
+                        if (assetType(property, 'props')) {
+                            genDefineProps(property)
+                        }
+
+                        if (assetType(property, 'data')) {
+                            genReactiveData(property)
+                        }
+
+                        if (assetType(property, 'computed')) {
+                            genComputed(property)
+                        }
+
+                        if (assetType(property, 'methods')) {
+                            genMethods(property)
+                        }
+                    })
+
+                    path.node.body.push(...bodyContent)
+                }
             }
         },
-        post (file) {
+        post(file) {
 
         }
     };
